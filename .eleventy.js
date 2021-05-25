fb21f62578c97116cde4130491af5ea53ed09646
Ben Myers <ben.myers@okstate.edu>
2021-05-25 12:30:48 -0500

    Get some basic pages going

---
+++ .eleventy.js
@@ -0,0 +1,7 @@
+module.exports = (eleventyConfig) => {
+	return {
+		dir: {
+			input: 'src'
+		}
+	}
+};
---
+++ src/index.md
@@ -0,0 +1,6 @@
+---
+layout: page.html
+title: Welcome to a11y.surf!
+---
+
+- [a11y](/en/a11y)
